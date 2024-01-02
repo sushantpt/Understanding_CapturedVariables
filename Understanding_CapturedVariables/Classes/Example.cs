09496d41e656454c7603285215dfd14a7083cf27
sushantpt <66266432+sushantpt@users.noreply.github.com>
2024-01-02 21:37:29 +0545

    Add project files.

---
+++ Understanding_CapturedVariables/Classes/Example.cs
@@ -0,0 +1,92 @@
+﻿namespace Understanding_CapturedVariables.Classes
+{
+    public class Example
+    {
+
+        public int externalVariable = 7;
+
+        // A delegate with lambda expression to get the total sum of a list.
+        public Func<List<int>, int> sumOfList = (listOfNums) => listOfNums.Sum();
+
+        // A delegate with lambda expression to get the total sum of odd numbers that are less than 6 a list.
+        public Func<List<int>, int> sumOfOddNums = (listOfNums) => listOfNums.Where(x => x < 6 && x % 2 != 0).Sum();
+
+
+        // captured variable.
+        public Func<int, int> squaredVal = x => x * x;
+
+
+        public Action<int> IncrementCounterAction(int incrementBy)
+        {
+            int defaultNumber = 2;
+            Action<int> action = input =>
+            {
+                Console.WriteLine($"Default counter value: {defaultNumber}");
+                int result = input + defaultNumber + incrementBy;
+                Console.WriteLine($"Increment action by (method argument): {incrementBy}");
+                Console.WriteLine($"Sum of method argument, lambda argument, and default local method value: {result}");
+            };
+            // update default number.
+            defaultNumber = 10; 
+            return action;
+        }
+
+
+        public void squaredValTest()
+        {
+            
+            Console.WriteLine($"External variable: {externalVariable}");
+
+            Console.WriteLine(squaredVal(externalVariable));
+
+            // modify externalVariable
+            externalVariable = 5;
+
+            Console.WriteLine($"External variable after modified to 5: {externalVariable}");
+
+            Console.WriteLine(squaredVal(externalVariable));
+        }
+
+
+        /// <summary>
+        /// Example of forming closures.
+        /// </summary>
+        /// <returns></returns>
+        public Action CreateCounterAction()
+        {
+            int incrementBy = 5;
+            // here, action doesnt take any arguments.
+            Action action = () => 
+            {
+                incrementBy++;
+                Console.WriteLine($"Counter at: {incrementBy}");
+            };
+
+            return action;
+        }
+
+
+
+        public Action[] ClosureActionInLoop()
+        {
+            Action[] actions = new Action[4];
+            for(int i = 0; i < 4; i++)
+            {
+                actions[i] = () => Console.WriteLine(i);
+            }
+            return actions;
+        }
+        
+        
+        public Action[] FixedClosureActionInLoop()
+        {
+            Action[] actions = new Action[4];
+            for(int i = 0; i < 4; i++)
+            {
+                int localVar = i; // introduce local variable to let closure remember
+                actions[i] = () => Console.WriteLine(localVar);
+            }
+            return actions;
+        }
+    }
+}
---
+++ Understanding_CapturedVariables/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Understanding_CapturedVariables.Classes;
+
+Console.WriteLine("Hello, World!");
+
+
+List<int> someNumbers = new List<int> { 1, 5, 6, 9, 4, 2, 7, 11, 3, 16 };
+Example eg = new Example();
+int sum = eg.sumOfList(someNumbers); // invokes the delegate which invokes the anonymous function.
+Console.WriteLine(sum);
+
+
+
+Console.WriteLine("Sum of odd numbers which are less than 6 in the list.");
+int sumOfOddNums = eg.sumOfOddNums(someNumbers);
+Console.WriteLine(sumOfOddNums);
+
+
+
+eg.squaredValTest();
+
+
+Action<int> action = eg.IncrementCounterAction(5);
+action(2);
+
+
+Console.WriteLine("---- Closure examples ----");
+
+// understanding closures
+
+Action closureAction = eg.CreateCounterAction();
+closureAction();
+closureAction();
+closureAction();
+closureAction();
+
+
+Console.WriteLine("---- Closure examples (in loop)----");
+
+
+Action[] closureLoopActions = eg.ClosureActionInLoop();
+foreach(var loopAction in closureLoopActions)
+{
+    loopAction();
+}
+
+Console.WriteLine("---- Closure examples (in loop: fixed)----");
+
+
+Action[] fixedClosureLoopActions = eg.FixedClosureActionInLoop();
+foreach(var loopAction in fixedClosureLoopActions)
+{
+    loopAction();
+}
